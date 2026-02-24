e1ba2958ca2631acc993deff9321730ed526dbfd
Kevin Joe Harris <kevinjoeharris1@gmail.com>
2026-02-25 02:38:06 +0530

    Testing CI
    Signed-off-by: Kevin Joe Harris <kevinjoeharris1@gmail.com>

--- hack/dev-mounter/main.go
+++ hack/dev-mounter/main.go
@@ -49,6 +49,7 @@ func newCommand() *cobra.Command {
 				cmNameToPath[parts[0]] = parts[1]
 			}
 
+			// Test commit
 			handledConfigMap := func(obj any) {
 				cm, ok := obj.(*corev1.ConfigMap)
 				if !ok {
